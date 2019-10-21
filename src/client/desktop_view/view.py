85a23c3c0522dd299e0cfd88f7f694825a3f294a
Pavel Antsypovich <pavelantsypovich@gmail.com>
2019-10-21 10:15:04 +0300

    -

--- src/client/desktop_view/view.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from client.desktop_view import gui
-
-
-class MessengerApp(QtWidgets.QMainWindow, gui.Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.gui = gui.Ui_MainWindow
--- src/server/controller/controller.py
+++
@@ -1,8 +0,0 @@
-from server.model import model
-
-
-class Controller:
-    def __init__(self, server_model: model.Server):
-        self.model = server_model
-
-    def getMessage(self):
